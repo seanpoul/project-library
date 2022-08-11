17fb157a06eb01f18852ee8c6d5042c664564653
Sean <seanpoulter@hotmail.com>
2022-08-10 21:30:50 -0400

    Add form to html and simple design to css

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const myLibrary = [{
-    title: "The Hobbit",
     author: "J.R.R. Tolkien",
+    title: "The Hobbit",
     pages: "295",
     read: true
 }, {
-    title: "I am legend",
     author: "Richard Matheson",
+    title: "I am legend",
     pages: "160",
     read: true
 }]
 
+// console.log(myLibrary[0].title)
+
+let addBook = document.getElementById('addBook');
+
+addBook.addEventListener('click', () => {
+
+})
+
+
 
 function Book(author, title, pages, read) {
-    
+
 }
 
 function addBookToLibrary() {
-    
+
 }
 
