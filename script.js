2360aeb42c06576ae84bcef674e920669fc7a0f9
Sean <seanpoulter@hotmail.com>
2022-08-10 20:24:47 -0400

    Start anew

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-let myLibrary = []
+const myLibrary = [{
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    pages: "295",
+    read: true
+}, {
+    title: "I am legend",
+    author: "Richard Matheson",
+    pages: "160",
+    read: true
+}]
+
 
 function Book(author, title, pages, read) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
-
+    
 }
 
-function addBookToLibrary(newBook) {
-    const table = document.getElementById("bookData");
-    newBook.forEach(item => {
-        let row = table.insertRow();
-        this.author = row.insertCell(0);
-        this.title = row.insertRow(1)
-        this.pages = row.insertRow(2)
-        this.read = row.insertRow(3)
-
-    });
+function addBookToLibrary() {
+    
 }
 
-
-
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "read")
